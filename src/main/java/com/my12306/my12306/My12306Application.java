5556f0cd245ab0246e68816b1fcd3905cbc8b2e5
caizhenghua <18101250556@163.com>
2023-11-07 17:09:02 +0800

    init

---
+++ src/main/java/com/my12306/my12306/My12306Application.java
@@ -0,0 +1,13 @@
+package com.my12306.my12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My12306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(My12306Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/my12306/my12306/My12306ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my12306.my12306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class My12306ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
